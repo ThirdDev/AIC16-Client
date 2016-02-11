8dcf0cd54a140dad4cb5ea5a462c7cc83453c23b
ahmadali shafiee <mail@ahmadalli.net>
2016-02-11 20:25:49 +0330

    constant.java added

---
+++ src/client/MST/constants.java
@@ -0,0 +1,9 @@
+package client.MST;
+
+/**
+ * Created by me on 11/02/2016.
+ */
+public class constants { //todo: rename coefficients
+    public static double c1 = 1/2;
+    public static double c2 = 3/4;
+}
