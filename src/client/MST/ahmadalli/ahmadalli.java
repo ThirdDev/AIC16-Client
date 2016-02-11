cf82766e6b9314e3ff7a65d487711dc40e181a2c
ahmadali shafiee <mail@ahmadalli.net>
2016-02-11 22:33:52 +0330

    layer1Move improvements

--- src/client/MST/ahmadalli/ahmadalli.java
+++ src/client/MST/ahmadalli/ahmadalli.java
@@ -53,24 +53,17 @@ public class ahmadalli {
         return 2;
     }
 
-    public static void layer1Move(World world, Node source, Node[] neighbours){
-        int myID = world.getMyID();
+    public static void layer1Move(World world, Node source){
+        Node weakest = null;
 
-        Node weakest = neighbours[0];
-
-        for (Node neighbor : neighbours) {
-            //will make sure that if there's an enemy node, the weakest will be the enemy
-            if (weakest.getOwner() == myID && neighbor.getOwner() != myID && neighbor.getOwner() != -1) {
-                weakest = neighbor;
-            }
-            if (neighbor.getOwner() != myID && neighbor.getArmyCount() > weakest.getArmyCount()) {
+        for (Node neighbor : getEnemyNeighbors(source,true)) {
+            if (weakest == null || neighbor.getArmyCount() > weakest.getArmyCount()) {
                 weakest = neighbor;
             }
         }
-
-        if (weakest.getOwner() != myID && weakest.getArmyCount() <= ahmadalli.getNodeState(source)) {
-            System.out.println("attack node " + source.getIndex() + " to weakest: " + weakest.getIndex());
-            world.moveArmy(source, weakest, (int) (source.getArmyCount() * constants.c1));
+        
+        if (weakest != null && weakest.getArmyCount() <= ahmadalli.getNodeState(source)) {
+            world.moveArmy(source, weakest, (int) ((double)source.getArmyCount() * constants.c1));
         } else {
             ArrayList<Node> friendlyNeighbors = ahmadalli.getFriendlyNeighbors(source, true);
             if (friendlyNeighbors.size() > 0) {
