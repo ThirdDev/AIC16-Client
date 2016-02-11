7fc11812754f3d994292833e679b0924bbf36c4f
ahmadali shafiee <mail@ahmadalli.net>
2016-02-11 20:25:39 +0330

    getEnemyNeighbors and getEnemyNeighbors methods

--- src/client/MST/ahmadalli/ahmadalli.java
+++ src/client/MST/ahmadalli/ahmadalli.java
@@ -1,5 +1,9 @@
 package client.MST.ahmadalli;
 
+import client.model.Node;
+
+import java.util.ArrayList;
+
 /**
  * Created by me on 11/02/2016.
  */
@@ -7,6 +11,34 @@ public class ahmadalli {
     public static void test()
     {
         System.out.println("It's a test");
+    }
+
+    public static Node[] getEnemyNeighbors(Node node)
+    {
+        ArrayList<Node> nodes= new ArrayList<>();
+        for(Node neighbor:node.getNeighbours())
+        {
+            if(neighbor.getOwner()!=node.getOwner())
+            {
+                nodes.add(neighbor);
+            }
+        }
+
+        return (Node[])nodes.toArray();
+    }
+
+    public static Node[] getFriendlyNeighbors(Node node)
+    {
+        ArrayList<Node> nodes= new ArrayList<>();
+        for(Node neighbor:node.getNeighbours())
+        {
+            if(neighbor.getOwner()==node.getOwner())
+            {
+                nodes.add(neighbor);
+            }
+        }
+
+        return (Node[])nodes.toArray();
     }
 }
 
