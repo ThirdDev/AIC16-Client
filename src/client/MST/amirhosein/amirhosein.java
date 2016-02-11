dda75b053f07b5fa128acbfe5e536b12f1d1cca8
ahmadali shafiee <mail@ahmadalli.net>
2016-02-11 13:34:36 +0330

    rename

---
+++ src/client/MST/amirhosein/amirhosein.java
@@ -0,0 +1,7 @@
+package client.MST.amirhosein;
+
+/**
+ * Created by me on 11/02/2016.
+ */
+public class amirhosein {
+}
--- src/client/MST/mirtaba/mirtaba.java
+++
@@ -1,7 +0,0 @@
-package client.MST.mirtaba;
-
-/**
- * Created by me on 11/02/2016.
- */
-public class mirtaba {
-}
