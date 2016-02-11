96119ec8efec8e46f6c4988da23c0fbba423f333
ahmadali shafiee <mail@ahmadalli.net>
2016-02-11 22:33:37 +0330

    make variables final

--- src/client/MST/constants.java
+++ src/client/MST/constants.java
@@ -3,10 +3,17 @@ package client.MST;
 /**
  * Created by me on 11/02/2016.
  */
-public class constants { //todo: rename coefficients
-    public static double c1 = 1/2;
-    public static double c2 = 3/4;
+public final class constants { //todo: rename coefficients
+    public static final double c1 = 1.0/2.0;
+    public static final double c2 = 3.0/4.0;
 
-    public static int c3 = 10;
-    public static int c4 = 30;
+    public static final int c3 = 10;
+    public static final int c4 = 30;
+
+    /**
+     The caller references the constants using <tt>Consts.EMPTY_STRING</tt>,
+     and so on. Thus, the caller should be prevented from constructing objects of
+     this class, by declaring this private constructor.
+     */
+    private constants(){ }
 }
