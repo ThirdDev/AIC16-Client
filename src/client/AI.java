1013223f1a9aaf2cc0ec9aa441cc68898559f662
ahmadali shafiee <mail@ahmadalli.net>
2016-02-15 17:25:22 +0330

    format codes

--- src/client/AI.java
+++ src/client/AI.java
@@ -16,9 +16,10 @@ import client.model.Node;
  * See World interface for more details.
  */
 public class AI {
-    Ahmadalli ahmadalli=new Ahmadalli();
-    Amirhosein amirhosein=new Amirhosein();
-    Mahdi mahdi=new Mahdi();
+    Ahmadalli ahmadalli = new Ahmadalli();
+    Amirhosein amirhosein = new Amirhosein();
+    Mahdi mahdi = new Mahdi();
+
     public void doTurn(World world) {
         // fill this method, we've presented a stupid AI for example!
         Node[] myNodes = world.getMyNodes();
@@ -27,21 +28,19 @@ public class AI {
             Node[] neighbours = source.getNeighbours();
             if (neighbours.length > 0) {
                 try {
-                    ahmadalli.layer1Move(world,source);
-                }
-                catch(Exception e) {
-                    moveRandomly(world,source,neighbours);
+                    ahmadalli.layer1Move(world, source);
+                } catch (Exception e) {
+                    moveRandomly(world, source, neighbours);
                 }
             }
         }
 
     }
 
-    private void moveRandomly(World world, Node source, Node[] neighbours)
-    {
+    private void moveRandomly(World world, Node source, Node[] neighbours) {
         // select a random neighbour
         Node destination = neighbours[(int) (neighbours.length * Math.random())];
         // move half of the node's army to the neighbor node
-        world.moveArmy(source, destination, source.getArmyCount()/2);
+        world.moveArmy(source, destination, source.getArmyCount() / 2);
     }
 }
--- src/client/MST/ahmadalli/ahmadalli.java
+++ src/client/MST/ahmadalli/ahmadalli.java
@@ -3,33 +3,17 @@ package client.MST.ahmadalli;
 import client.World;
 import client.model.Node;
 import client.MST.constants;
+
 import java.util.ArrayList;
 
 /**
  * Created by me on 11/02/2016.
  */
 public class Ahmadalli {
-    public  ArrayList<Node> getEnemyNeighbors(Node node, boolean emptyNeighbors)
-    {
-        ArrayList<Node> nodes= new ArrayList<>();
-        for(Node neighbor:node.getNeighbours())
-        {
-            if(neighbor.getOwner()!=node.getOwner()|| (emptyNeighbors&&neighbor.getOwner()==-1))
-            {
-                nodes.add(neighbor);
-            }
-        }
-
-       return nodes;
-    }
-
-    public  ArrayList<Node> getFriendlyNeighbors(Node node, boolean emptyNeighbors)
-    {
-        ArrayList<Node> nodes= new ArrayList<>();
-        for(Node neighbor:node.getNeighbours())
-        {
-            if(neighbor.getOwner()==node.getOwner()||(emptyNeighbors&&neighbor.getOwner()==-1))
-            {
+    public ArrayList<Node> getEnemyNeighbors(Node node, boolean emptyNeighbors) {
+        ArrayList<Node> nodes = new ArrayList<>();
+        for (Node neighbor : node.getNeighbours()) {
+            if (neighbor.getOwner() != node.getOwner() || (emptyNeighbors && neighbor.getOwner() == -1)) {
                 nodes.add(neighbor);
             }
         }
@@ -37,27 +21,37 @@ public class Ahmadalli {
         return nodes;
     }
 
-    public  int getNodeState(Node node)
-    {
+    public ArrayList<Node> getFriendlyNeighbors(Node node, boolean emptyNeighbors) {
+        ArrayList<Node> nodes = new ArrayList<>();
+        for (Node neighbor : node.getNeighbours()) {
+            if (neighbor.getOwner() == node.getOwner() || (emptyNeighbors && neighbor.getOwner() == -1)) {
+                nodes.add(neighbor);
+            }
+        }
+
+        return nodes;
+    }
+
+    public int getNodeState(Node node) {
         int armyCount = node.getArmyCount();
-        if(armyCount <= constants.c3)
+        if (armyCount <= constants.c3)
             return 0;
-        if(armyCount <= constants.c4)
+        if (armyCount <= constants.c4)
             return 1;
         return 2;
     }
 
-    public  void layer1Move(World world, Node source){
+    public void layer1Move(World world, Node source) {
         Node weakest = null;
 
-        for (Node neighbor : getEnemyNeighbors(source,true)) {
+        for (Node neighbor : getEnemyNeighbors(source, true)) {
             if (weakest == null || neighbor.getArmyCount() > weakest.getArmyCount()) {
                 weakest = neighbor;
             }
         }
 
         if (weakest != null && weakest.getArmyCount() <= getNodeState(source)) {
-            world.moveArmy(source, weakest, (int) ((double)source.getArmyCount() * constants.c1));
+            world.moveArmy(source, weakest, (int) ((double) source.getArmyCount() * constants.c1));
         } else {
             ArrayList<Node> friendlyNeighbors = getFriendlyNeighbors(source, true);
             if (friendlyNeighbors.size() > 0) {
@@ -67,34 +61,30 @@ public class Ahmadalli {
         }
     }
 
-    public  boolean isBorderNode(Node node)
-    {
+    public boolean isBorderNode(Node node) {
         try {
-            int ownerId=node.getOwner();
-            for(Node neighborNode:node.getNeighbours())
-            {
-                if(neighborNode.getOwner()!=ownerId)
+            int ownerId = node.getOwner();
+            for (Node neighborNode : node.getNeighbours()) {
+                if (neighborNode.getOwner() != ownerId)
                     return true;
             }
+        } catch (Exception e) {
         }
-        catch (Exception e) {}
         return false;
 
     }
 
-    public ArrayList<Node> getBorderNodes(World world)
-    {
-        ArrayList<Node> nodes=new ArrayList<>();
-        for(Node node:world.getMyNodes()){
-            if(isBorderNode(node))
-            {
+    public ArrayList<Node> getBorderNodes(World world) {
+        ArrayList<Node> nodes = new ArrayList<>();
+        for (Node node : world.getMyNodes()) {
+            if (isBorderNode(node)) {
                 nodes.add(node);
             }
         }
         return nodes;
     }
-    
-    public static void logException(Exception e){
+
+    public static void logException(Exception e) {
         System.out.println(e.getMessage());
     }
 
