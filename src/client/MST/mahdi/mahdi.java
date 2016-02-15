8cd301154b723b9acbac9f9dbe4e23ff4b1d3bfa
Mahdi <mghiasi75@gmail.com>
2016-02-15 18:03:10 +0330

    minimumRecommendedForceInBorders

--- src/client/MST/mahdi/mahdi.java
+++ src/client/MST/mahdi/mahdi.java
@@ -1,7 +1,23 @@
 package client.MST.mahdi;
 
+import client.model.Node;
+
+import java.util.ArrayList;
+
 /**
  * Created by me on 11/02/2016.
  */
 public class Mahdi {
+
+    int minimumRecommendedForceInBorders = 10;
+    int minimumRecommendedForceInBordersFailSafe = 200;
+
+    public int getMinimumRecommendedForceInBorders() {
+        return minimumRecommendedForceInBorders;
+    }
+
+    public int increaseMinimumRecommendedForceInBorders() {
+        minimumRecommendedForceInBorders++;
+    }
+
 }
