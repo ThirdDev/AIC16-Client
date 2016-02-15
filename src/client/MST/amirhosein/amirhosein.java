43f8d6b589f66e4254692f5c8ae24c7889cb433b
Amirhossein <mirtaba@outlook.com>
2016-02-15 20:45:25 +0330

    findDis method is finding dis to nearest boarderNode

--- src/client/MST/amirhosein/amirhosein.java
+++ src/client/MST/amirhosein/amirhosein.java
@@ -96,58 +96,46 @@ public class Amirhosein
 
     public static Map<Node,Integer> findDis (World world , ArrayList<Node> borderNodes)
     {
-        int nextTurnVals[] = new int[200];
-        for(int j = 0 ; j <world.getMyNodes().length;j++)
+        int dis[] = new int[150];
+        for(int j = 0 ; j < 150; j++)
         {
-            Node u = world.getMyNodes()[j];
-            nextTurnVals[u.getIndex()] = u.getArmyCount();
+            dis[j] = Integer.MAX_VALUE;
         }
+
         Queue <Node> q = new LinkedList<Node>();
         for (int i = 0 ; i < borderNodes.size(); i++)
         {
+            q.clear();
             boolean flag = false;
             Node chief = borderNodes.get(i);
             q.add(chief);
-            int dis[] = new int[150];
-            for(int j = 0 ; j < 150; j++)
-            {
-                dis[j] = 0;
-            }
-            while(q.size()>0 && !flag)
+            while(q.size()>0)
             {
                 Node u = q.poll();
+                int mark[] = new int[150];
+                for(int j = 0 ; j < 150; j++)
+                {
+                    mark[j] = 0;
+                }
+                mark[u.getIndex()]=1;
                 for(int j = 0 ; j < u.getNeighbours().length; j++ )
                 {
                     Node neighbour = u.getNeighbours()[j];
-                    if(neighbour.getOwner()==world.getMyID() &&  dis[neighbour.getIndex()] == 0)
+                    if(neighbour.getOwner() == world.getMyID() &&  mark[neighbour.getIndex()] == 0)
                     {
                         q.add(neighbour);
-                        dis[neighbour.getIndex()] = dis[u.getIndex()] + 1;
-                        if(!Ahmadalli.isBorderNode(neighbour)) //TODO: Leave a Minimum of one
-                        {
-                            if( nextTurnVals[neighbour.getIndex()]> 1)
-                            {
-                                nextTurnVals[neighbour.getIndex()] =1;
-                                nextTurnVals[u.getIndex()] += neighbour.getArmyCount()-1;
-                                world.moveArmy(neighbour,u,neighbour.getArmyCount()-1);
-                                flag = true;
-                                break;
-                            }
-                        }
-                        else
-                        {
-                            if( nextTurnVals[neighbour.getIndex()]> Mahdi.getMinimumRecommendedForceInBorders())
-                            {
-                                nextTurnVals[neighbour.getIndex()] = Mahdi.getMinimumRecommendedForceInBorders();
-                                nextTurnVals[u.getIndex()] += neighbour.getArmyCount()-Mahdi.getMinimumRecommendedForceInBorders();
-                                world.moveArmy(neighbour,u,neighbour.getArmyCount()-Mahdi.getMinimumRecommendedForceInBorders());
-                                flag = true;
-                                break;
-                            }
-                        }
+                        mark[neighbour.getIndex()] =1;
+                        dis[neighbour.getIndex()] = Math.min( dis[neighbour.getIndex()] , (dis[u.getIndex()] + 1) );
                     }
                 }
             }
         }
+        Map <Node ,  Integer > ret = new HashMap<>();
+        Graph map = world.getMap();
+        for(Node node:map.getNodes())
+        {
+            ret.put(node, dis[node.getIndex()]);
+        }
+        return ret;
     }
 }
